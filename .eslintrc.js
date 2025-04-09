e66274b3e283f34953013ef52f5947f94a145a75
Federico Piccinno <federicopiccinno@gmail.com>
2025-04-09 12:32:52 +0200

    Correzione errori ESLint e TypeScript, aggiornamento dipendenze

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,19 @@
 module.exports = {
-  extends: ['next/core-web-vitals'],
+  extends: [
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   rules: {
     'react/no-unescaped-entities': 'off',
     '@next/next/no-img-element': 'off',
     'react-hooks/exhaustive-deps': 'warn',
-    'no-unused-vars': 'warn',
-    '@typescript-eslint/no-unused-vars': 'warn',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', {
+      'argsIgnorePattern': '^_',
+      'varsIgnorePattern': '^_'
+    }]
   },
   settings: {
     next: {
--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,10 @@ import TestimonialsCarousel from '@/components/TestimonialsCarousel'
 export default function Home() {
   return (
     <div className="min-h-screen bg-background">
-      <Hero />
+      <Hero 
+        title="L'intelligenza artificiale o il sito dei tuoi sogni, creati su misura per te."
+        subtitle="Mini App AI potenti + Progetti Web e AI personalizzati."
+      />
       <CustomServicesSection />
       <TestimonialsCarousel />
       <Footer />
