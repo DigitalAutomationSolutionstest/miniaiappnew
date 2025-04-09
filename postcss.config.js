2c44a906000277fc689b536c691c5babc8f3fd15
Federico Piccinno <federicopiccinno@gmail.com>
2025-04-09 14:06:33 +0200

    Risolto problema con tailwindcss/nesting e aggiunto componenti toast

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   plugins: {
-    'postcss-nesting': {},
-    tailwindcss: {},
-    autoprefixer: {},
-    cssnano: {
+    "postcss-nesting": {},
+    "tailwindcss": {},
+    "autoprefixer": {},
+    "cssnano": {
       preset: 'default',
     }
   },
